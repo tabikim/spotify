d3aa85e1cae1b9bce08374ad254453f91d0d8f5e
Tabitha Kim <tabijkim@gmail.com>
2015-11-10 17:53:08 -0800

    album artwork, re-designed page, added login/register form, modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# spotify
+#spotify
+
+1. What is the purpose of your project (i.e., why would someone use this app)?
+
+	My project allows you to create or login to your spotify account and look for tracks related to the input as well as preview the track.
+
+2. What is the URL of this project on your student web-server?
+
+	https://students.washington.edu/tabikim/info343/spotify
+
+3. Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+
+	Mason and I worked together a bit!
+
+4. Approximately how many hours did it take you to complete this challenge?
+
+	~15 hours
+
+5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+	
+	The most difficult part was implementing the spotify web api end reference points. Also, deciding on what to do for this project. I think it would be better to give more clarification on how to implement web API's before us handling one. 
+
---
+++ app.js
@@ -0,0 +1,91 @@
+var data;
+var playlistData;
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var playlistUrl = 'https://api.spotify.com/v1/browse/featured-playlists'
+var myApp = angular.module('myApp', ['firebase'])
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http, $firebaseAuth, $firebaseObject) {
+
+  var ref = new Firebase('https://tabsspotify.firebaseio.com/');
+
+  var userRef = ref.child("users");
+
+  $scope.users = $firebaseObject(ref);
+  $scope.authObj = $firebaseAuth(ref);
+
+  var authData = $scope.authObj.$getAuth();
+  if (authData) {
+    $scope.userId = authData.uid;
+  }
+
+  // Register function
+  $scope.register = function() {
+    $scope.authObj.$createUser({
+      email: $scope.email,
+      password: $scope.password,
+    })
+
+    .then($scope.logIn)
+
+    .then(function(authData) {
+      $scope.userId = authData.uid;
+      $scope.users[authData.uid] ={
+        username: $scope.username,
+        userImg: $scope.userImg,
+      }
+      $scope.users.$save()
+    })
+
+    .catch(function(error) {
+      console.error("Error: ", error);
+    });
+  }
+
+  $scope.signIn = function() {
+    $scope.logIn().then(function(authData){
+      $scope.userId = authData.uid;
+    })
+  }
+
+  // LogIn function
+  $scope.logIn = function() {
+    console.log('log in')
+    return $scope.authObj.$authWithPassword({
+      email: $scope.email,
+      password: $scope.password
+    })
+  }
+
+  // LogOut function
+  $scope.logOut = function() {
+    $scope.authObj.$unauth()
+    $scope.userId = false
+  }
+
+  $scope.audioObject = {}
+  $scope.getSongs = function() {
+    $http.get(baseUrl + $scope.track).success(function(response) {
+      data = $scope.tracks = response.tracks.items
+    })
+  }
+
+  $scope.play = function(song) {
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause()
+      $scope.currentSong = false
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play()  
+      $scope.currentSong = song
+    }
+  }
+})
+
+
+// Add tool tips to anything with a title property
+$('body').tooltip({
+    selector: '[title]'
+});
